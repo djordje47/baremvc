eb6db2aadd2580a2d68341b79624365e4be869c9
Djordje Arsenovic <dj.arsenovic@gmail.com>
2019-11-14 17:20:18 +0100

    Added functionality of requiring and instantiating controllers, and calling a methods in them - based on urls. Updated Pages controller.

--- app/controllers/Pages.php
+++ app/controllers/Pages.php
@@ -4,6 +4,16 @@ class Pages
 {
     public function __construct()
     {
-        echo 'Pages loaded';
+        echo 'Pages loaded <br>';
+    }
+
+    public function index()
+    {
+        echo 'Index method <br>';
+    }
+
+    public function about($id)
+    {
+        echo 'This is about ' . $id;
     }
 }
--- app/libraries/Core.php
+++ app/libraries/Core.php
@@ -24,6 +24,7 @@ class Core
      */
     protected $params = [];
 
+    // TODO Clean up constructor create separate private function one that will load controllers and one that will call methods @Djole
     public function __construct()
     {
         $url = $this->getUrl();
@@ -37,6 +38,16 @@ class Core
         require_once '../app/controllers/' . $this->currentController . '.php';
 
         $this->currentController = new $this->currentController;
+
+        if (isset($url[1])) {
+            if (method_exists($this->currentController, $url[1])) {
+                $this->currentMethod = $url[1];
+                unset($url[1]);
+            }
+        }
+        $this->params = $url ? array_values($url) : [];
+
+        call_user_func_array([$this->currentController, $this->currentMethod], $this->params);
     }
 
     /**
